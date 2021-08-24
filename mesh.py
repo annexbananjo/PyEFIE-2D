caafa3ef78fa4ffef75e06aee98603abab3c3399
Elia A. Attardo <attardo@altair.com>
2021-08-24 14:37:33 +0200

    Adding FAST mat-vec

--- mesh.py
+++ mesh.py
@@ -16,9 +16,12 @@ class Mesh():
         self.an = 0.0  # resolution/2 of the mesh
         # key is the pos in the vector of primitiveObj, value is idObj
         self.idBackground = {0: -1}
+    #--------------------------------------------
+
 
     def __len__(self):
         return len(self.cells)
+    #--------------------------------------------
 
     def __buildKdTree(self):
         # Build the kd-tree for all mid-points of the mesh
@@ -29,6 +32,7 @@ class Mesh():
             cellCoords[i, :] = np.array([self.cells[i].x, self.cells[i].y])
             # build the tree
         self.tree = spatial.KDTree(cellCoords)
+    #--------------------------------------------
 
     # Query if a point is within a cell
     def pointInCell(self, robs):
@@ -44,6 +48,7 @@ class Mesh():
         point[1] = robs.y
         d_cell = self.tree.query(point)
         return d_cell[0], d_cell[1]
+    #--------------------------------------------
 
     def plotMesh(self):
         # Extract x and y from cells
@@ -56,30 +61,7 @@ class Mesh():
             y.append(self.cells[i].y)
         plt.plot(x, y, 'k+')
         plt.show()
-
-    # Private members function
-    def __createMeshFromBackgroundCircle(self, circle, resolution):
-        if resolution == 0.0:
-            raise ValueError("Resolution cannot be zero")
-        self.an = resolution / 2
-        # We assume that circle is envoloped within an equivalent square
-        xmin = circle.center.x - circle.radius
-        xmax = circle.center.x + circle.radius
-        ymin = circle.center.y - circle.radius
-        ymax = circle.center.y + circle.radius
-        # Compute the # of cells in x and y
-        nx = int(round((xmax - xmin + resolution) / resolution))
-        ny = int(round((ymax - ymin + resolution) / resolution))
-        if nx != ny:
-            raise ValueError("Circle must be envoled within a square")
-        x = np.linspace(xmin, xmax, nx)
-        y = np.linspace(ymin, ymax, ny)
-        # Loop and set the cells
-        for i in range(nx):
-            for j in range(ny):
-                aPt = Point2D(x[i], y[j])
-                if compute_distance(aPt, circle.center) < circle.radius:
-                    self.cells.append(aPt)
+    #--------------------------------------------
 
     def __createMeshFromBackgroundRectangle(self, rectangle, resolution):
         if resolution == 0.0:
@@ -90,17 +72,17 @@ class Mesh():
         ymin = rectangle.pMin.y
         ymax = rectangle.pMax.y
         # Compute the # of cells in x and y
-        nx = int(round((xmax - xmin + resolution) / resolution))
-        ny = int(round((ymax - ymin + resolution) / resolution))
-        if nx != ny:
-            raise ValueError("Circle must be envoled within a square")
-        x = np.linspace(xmin, xmax, nx)
-        y = np.linspace(ymin, ymax, ny)
+        self.nx = int(round((xmax - xmin + resolution) / resolution))
+        self.ny = int(round((ymax - ymin + resolution) / resolution))
+        # Create the grid points
+        x = np.linspace(xmin, xmax, self.nx)
+        y = np.linspace(ymin, ymax, self.ny)
         # Loop and set the cells
-        for i in range(nx):
-            for j in range(ny):
+        for i in range(self.nx):
+            for j in range(self.ny):
                 aPt = Point2D(x[i], y[j])
                 self.cells.append(aPt)
+    #--------------------------------------------
 
     def generateMesh(self, listPrimitiveObj, resolution):
         """ This function generate the mesh given a list of primitive object
@@ -121,34 +103,37 @@ class Mesh():
         if type(listPrimitiveObj[keyMin]) is Rectangle:
             self.__createMeshFromBackgroundRectangle(
                 listPrimitiveObj[keyMin], resolution)
-        elif type(listPrimitiveObj[keyMin]) is Circle:
-            self.__createMeshFromBackgroundCircle(
-                listPrimitiveObj[keyMin], resolution)
         else:
             raise RuntimeError("Wrong background object")
         # Build the tree
         self.__buildKdTree()
+    #--------------------------------------------
 
     def __str__(self):
-        """ Print the mesh
+        """ Printing the mesh
         """
         sMesh = 'Mesh:'
+        sMesh += "\n"
+        sMesh += 'Nx: {}'.format(self.nx)
+        sMesh += "\n"
+        sMesh += 'Ny: {}'.format(self.ny)
         sMesh += "\n"
         for i in range(len(self.cells)):
             sMesh += '{}: {}'.format(i, self.cells[i])
             sMesh += "\n"
         return sMesh
+    #--------------------------------------------
 
-
+# Quickly test
 if __name__ == '__main__':
 
     rec1 = Rectangle(idObj=2)
 
     pMin = Point2D(0, 0)
     pMax = Point2D(2, 2)
-    rec2 = Rectangle(idObj=3, pMin=pMin, pMax=pMax)
+    rec2 = Rectangle(idObj=1, pMin=pMin, pMax=pMax)
     circ1 = Circle(idObj=4)
-    circ2 = Circle(idObj=1, radius=2.0, center=Point2D(0.0, 0.0))
+    circ2 = Circle(idObj=3, radius=0.5, center=Point2D(0.0, 0.0))
     # append a couple of mix objs
     print("\n")
     vPrObj = []
@@ -163,3 +148,4 @@ if __name__ == '__main__':
     msh.plotMesh()
     print(msh.pointInCell(Point2D()))
     print('No. of cells in the mesh: {}'.format(len(msh)))
+    print(msh)
